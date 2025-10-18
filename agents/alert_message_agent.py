a5bc95d82dbf1452908e0a280ea45ecd1b8a57d6
Maria Nadeem <146998477+marianadeem755@users.noreply.github.com>
2025-10-18 20:06:59 +0500

    Update alert_message_agent.py

--- agents/alert_message_agent.py
+++ agents/alert_message_agent.py
@@ -1,5 +1,5 @@
 from langchain_groq import ChatGroq
-from langchain.schema.output_parser import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
 from config import GROQ_API_KEY
 from datetime import datetime
